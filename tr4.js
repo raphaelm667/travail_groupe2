3417386fea51020504f1645b2b96bf3689c3b237
raphaelm667 <71373096+raphaelm667@users.noreply.github.com>
2020-10-14 11:50:14 +0200

    Update tr4.js

--- tr4.js
+++ tr4.js
@@ -1,3 +1,41 @@
-sjdbrhiuerghiudhrguih
-T1Ps
-efzhujfhes
+score = [[Math.random()*8,Math.random()*8],[Math.random()*8,Math.random()*8],[Math.random()*8,Math.random()*8]];
+equipe = ["PSG","Manchester","Liverpool","OM","OL","FCB"];
+let victoire = [];
+
+function match{
+if(score[0][0]>score[0][1])
+  {
+    victoire[0][0] = score[0][0];
+    victoire[0][1] = "PSG";
+  }
+else {
+  victoire[0][0] = score[0][1];
+  victoire[0][1] = "Manchester";
+}
+if(score[1][0]>score[1][1])
+  {
+    victoire[1][0] = score[1][0];
+    victoire[1][1] = "Livepool";
+  }
+else {
+  victoire[1][0] = score[1][1];
+  victoire[1][1] = "OM";
+}
+if(score[2][0]>score[2][1])
+  {
+    victoire[2][0] = score[2][0];
+    victoire[2][1] = "OL";
+  }
+else {
+  victoire[2][0] = score[2][1];
+  victoire[2][1] = "FCB";
+}
+}
+
+function affichage{
+console.log("Resultat du match 1:"+equipe[1]+"contre"+equipe[2]+" victoire"+victoire[0][1]+" :"+victoire[0][0]);
+console.log("Resultat du match 2:"+equipe[3]+"contre"+equipe[4]+" victoire"+victoire[1][1]+" :"+victoire[1][0]);
+console.log("Resultat du match 3:"+equipe[5]+"contre"+equipe[6]+" victoire"+victoire[2][1]+" :"+victoire[2][0]);
+
+  
+}
