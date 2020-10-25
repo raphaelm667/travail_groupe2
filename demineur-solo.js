debfef82070a263897697a61699f27af5bb96be1
Jarodbf <71373525+Jarodbf@users.noreply.github.com>
2020-10-25 18:51:19 +0100

    Add files via upload

--- demineur-solo.js
+++ demineur-solo.js
@@ -1,27 +1,27 @@
 let plateau = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]];
 let nbrVie = 4;
-function nbBomb()
+function nbBombe()
 {
-let nbrBombe = +prompt("combien de bombes sur le terrain ?");
-for(let i = 0; i <= nbrBomb; i++){
-    let posBomb1 = Math.floor(Math.random()*5);
-    let posBomb2 = Math.floor(Math.random()*5);
-    while (plateau[posBomb1][posBomb2] !== 0){
-        posBomb1 = Math.floor(Math.random()*5);
-        posBomb2 = Math.floor(Math.random()*5);
-    }
-    if (plateau[posBomb1][posBomb2] === 0){
-        plateau[posBomb1][posBomb2] = 2;
-    }
-  }
-}
+	let nbrBombe = +prompt("combien de bombes sur le terrain ?");
+	for(let i = 0; i <= nbrBombe; i++){
+		let posBomb1 = Math.floor(Math.random()*5);
+		let posBomb2 = Math.floor(Math.random()*5);
+		while (plateau[posBomb1][posBomb2] !== 0){
+			posBomb1 = Math.floor(Math.random()*5);
+			posBomb2 = Math.floor(Math.random()*5);
+		}
+		if (plateau[posBomb1][posBomb2] === 0){
+			plateau[posBomb1][posBomb2] = 2;
+		}
+	  }
+	}
 
-for (let i = 0; i < 5; i++){
-    for (let j = 0; j < 5; j++) {
-        if (plateau[i][j] === 0){
-            plateau[i][j] = 1;
-        }
-    }
+	for (let i = 0; i < 5; i++){
+		for (let j = 0; j < 5; j++) {
+			if (plateau[i][j] === 0){
+				plateau[i][j] = 1;
+			}
+		}
 }
 
 function testVal(ligne, colone){
