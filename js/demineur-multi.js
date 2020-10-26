0866432c8bf8c46bbcdf9d314a457c5f2c78ca25
quentinrld <71373028+quentinrld@users.noreply.github.com>
2020-10-26 17:11:40 +0100

    Update demineur-multi.js

--- js/demineur-multi.js
+++ js/demineur-multi.js
@@ -1,7 +1,6 @@
 let plateau = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]];
 let tableJoueur = [[0,3],[0,3],[0,3],[0,3]];
 let nbJoueur = +prompt("combien de joueurs ?"),maxTour=0;
-let maxTour = 0;
 
 function nbBombe(){
 let nbrBombe = +prompt("combien de bombes sur le terrain ?");
